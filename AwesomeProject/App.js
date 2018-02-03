8907af1f5d8b6e82d4542421d611cff0b89a9c42
Gavin Young <gavinyoung1994@gmail.com>
2018-02-03 05:26:04 +0000

    login page works now

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,16 +1,11 @@
-/**[]
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
   View,
   Image,
-  TextInput
+  TextInput,
+  Button
 } from 'react-native';
 
 export default class App extends React.Component {
@@ -18,28 +13,77 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       username: '',
-      password: ''
+      password: '',
+      isLoading: false
     }
+  }
+
+  loginOnClick = () => {
+    this.setState({isLoading: true});
+    fetch(`https://api-wufthacks.xlabs.one:8243/userSignin/V1.0.0/signin`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer 8b60815e-85d1-3a21-9154-6c384535a6f2',
+          'Content-Type': 'application/json'
+
+        },
+        body: JSON.stringify({
+           'username': this.state.username,
+           'password': this.state.password
+        })
+      })
+    .then((response) => {
+      if(response.status == 200){
+        return response.json();
+      }else{
+        alert(response);
+        this.setState({isLoading: false});
+      }
+    }).then((responseData) => {
+      alert(responseData["X-Api-Key"]);
+      this.setState({isLoading: false});
+    })
+    .catch((error) => {
+      alert(error);
+      this.setState({isLoading: false});
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
           <View style={{alignItems: 'center'}}>
-            <View style={{flexDirection: 'row', marginBottom: 40}}>
-              <Text style={{fontSize: 24}}>TD Bank Payment System</Text>
+            <View style={{flexDirection: 'row', marginBottom: 20}}>
+              <Text style={{fontSize: 24, color: '#5dd55d'}}>TD Bank Payment System</Text>
             </View>
-            <TextInput
-              style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 180}}
-              onChangeText={(text) => this.setState({username: text})}
-              value={this.state.username}
-            />
-            <TextInput
-              secureTextEntry = {true}
-              style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 180}}
-              onChangeText={(text) => this.setState({password: text})}
-              value={this.state.password}
-            />
+            <View>
+              <Text style={{textAlign: 'center'}}>
+                Username{'\n'}
+              </Text>
+              <TextInput
+                style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 180, borderRadius: 10}}
+                underlineColorAndroid = 'rgba(0,0,0,0)'
+                onChangeText={(text) => this.setState({username: text})}
+                value={this.state.username}
+              />
+              <Text style={{textAlign: 'center'}}>
+                {'\n'}Password{'\n'}
+              </Text>
+              <TextInput
+                secureTextEntry = {true}
+                underlineColorAndroid = 'rgba(0,0,0,0)'
+                style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 180, marginBottom: 20, borderRadius: 10}}
+                onChangeText={(text) => this.setState({password: text})}
+                value={this.state.password}
+              />
+              <Button
+                onPress={this.loginOnClick}
+                color="#5dd55d"
+                title='Login'
+                disabled={this.state.isLoading}
+              />
+            </View>
           </View>
       </View>
     );
@@ -49,8 +93,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   }
 });
